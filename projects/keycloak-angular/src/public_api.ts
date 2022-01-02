fd4a4d2b9e772db1176e5c25216d99e98496b235
Hamid Tabakhha <44755537+Hamid-Tabakhha@users.noreply.github.com>
2022-01-02 11:57:06 +0330

    Update public_api.ts
    public_api.ts updated

--- projects/keycloak-angular/src/public_api.ts
+++ projects/keycloak-angular/src/public_api.ts
@@ -16,3 +16,4 @@ export { KeycloakService } from './lib/core/services/keycloak.service';
 export { KeycloakBearerInterceptor } from './lib/core/interceptors/keycloak-bearer.interceptor';
 export { CoreModule } from './lib/core/core.module';
 export { KeycloakAngularModule } from './lib/keycloak-angular.module';
+export { UserIdentity } from './lib/core/interfaces/keycloak-ret';
